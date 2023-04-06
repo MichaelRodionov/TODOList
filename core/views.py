27064fd0be5fda13b25ec228655bb9c3b5cfd133
Michael Rodionov <rodionovspb9618@mail.ru>
2023-04-06 21:10:04 +0300

    fix view

--- core/views.py
+++ core/views.py
@@ -1,10 +1,11 @@
 from django.contrib.auth import authenticate, login
 from rest_framework import serializers
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, RetrieveAPIView
+from rest_framework.views import APIView
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 from core.models import User
-from core.serializers import UserRegistrationSerializer
+from core.serializers import UserRegistrationSerializer, UserDetailSerializer
 
 
 # ----------------------------------------------------------------
@@ -15,13 +16,17 @@ class UserCreateView(CreateAPIView):
 
 
 class UserLoginView(TokenObtainPairView):
-
     def post(self, request, *args, **kwargs):
         user = authenticate(
             username=request.data.get('username'),
             password=request.data.get('password')
         )
-        if not user:
-            raise serializers.ValidationError('Check your username or password')
+        if user is None:
+            raise serializers.ValidationError('Invalid username or password')
         login(request, user)
         return super().post(request, *args, **kwargs)
+
+
+class UserDetailView(RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer = UserDetailSerializer
