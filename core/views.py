8c419adba077c27052ee9506dc5685211fa6c434
Michael Rodionov <rodionovspb9618@mail.ru>
2023-04-06 21:15:48 +0300

    fix view 1

--- core/views.py
+++ core/views.py
@@ -1,7 +1,6 @@
 from django.contrib.auth import authenticate, login
-from rest_framework import serializers
+from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.generics import CreateAPIView, RetrieveAPIView
-from rest_framework.views import APIView
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 from core.models import User
@@ -22,7 +21,7 @@ class UserLoginView(TokenObtainPairView):
             password=request.data.get('password')
         )
         if user is None:
-            raise serializers.ValidationError('Invalid username or password')
+            raise AuthenticationFailed('Invalid username or password')
         login(request, user)
         return super().post(request, *args, **kwargs)
 
