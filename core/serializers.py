834ef017ba96cd1bc23b43010896cc5c0696b44a
Michael Rodionov <rodionovspb9618@mail.ru>
2023-04-06 18:30:03 +0300

    fix serializer

--- core/serializers.py
+++ core/serializers.py
@@ -1,20 +1,26 @@
+import email
+
+from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
 
 from core.models import User
 
 
+# ----------------------------------------------------------------
+# user serializers
 class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(required=True)
+    password_repeat = serializers.CharField(write_only=True)
 
     class Meta:
         model: User = User
-        fields = '__all__'
+        fields = ('username', 'first_name', 'last_name', 'email', 'password', 'password_repeat')
 
-    def validate(self, data):
-        print(data)
-        if data.get('password') != data.get('password_repeat'):
+    def validate(self, attrs):
+        if attrs.get('password') != attrs.pop('password_repeat'):
             raise serializers.ValidationError('Password mismatch')
-        data.pop('password_repeat')
-        return data
+        validate_password(attrs.get('password'))
+        return attrs
 
     def create(self, validated_data):
         user = super().create(validated_data)
