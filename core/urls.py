f5a561c784a2777f032812e96b77c376097553be
Michael Rodionov <rodionovspb9618@mail.ru>
2023-04-06 20:27:46 +0300

    fix route

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from core.views import UserCreateView
+from core.views import UserCreateView, UserLoginView
 
 
 # ----------------------------------------------------------------
 # urlpatterns
 urlpatterns = [
     path('signup', UserCreateView.as_view()),
-    path('login', UserCreateView.as_view)
+    path('login', UserLoginView.as_view())
 ]
