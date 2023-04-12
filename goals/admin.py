deffdbb6ce57ced7053dd743b49bc81fd05626b9
Michael Rodionov <rodionovspb9618@mail.ru>
2023-04-12 22:31:32 +0300

    redefine admin fieldsets

--- goals/admin.py
+++ goals/admin.py
@@ -6,18 +6,54 @@ from goals import models
 # ----------------------------------------------------------------
 # admin models
 class GoalCategoryAdmin(admin.ModelAdmin):
-    list_display = ('title', 'user', 'created', 'updated')
+    list_display = ('title', 'user', 'created', 'updated', 'is_deleted')
     search_fields = ('title', 'user')
+    readonly_fields = ('created', 'updated')
+
+    fieldsets = (
+        ('Info', {
+            'fields': ('title', 'user', 'created', 'updated')
+        }),
+        ('Dates', {
+            'fields': ('created', 'updated')
+        }),
+        ('Status', {
+            'fields': ('is_deleted',)
+        }),
+    )
 
 
 class GoalAdmin(admin.ModelAdmin):
-    list_display = ('title', 'user', 'created', 'updated', 'category')
-    search_fields = ('title', 'user', 'category')
+    list_display = ('title', 'user', 'created', 'updated', 'category', 'due_date', 'priority', 'status')
+    search_fields = ('title', 'user', 'category', 'due_date', 'priority', 'status')
+    readonly_fields = ('created', 'updated')
+
+    fieldsets = (
+        ('Info', {
+            'fields': ('title', 'user', 'category', 'priority')
+        }),
+        ('Dates', {
+            'fields': ('created', 'updated', 'due_date')
+        }),
+        ('Status', {
+            'fields': ('status',)
+        }),
+    )
 
 
 class CommentAdmin(admin.ModelAdmin):
     list_display = ('text', 'user', 'created', 'updated', 'goal')
     search_fields = ('text', 'user', 'goal')
+    readonly_fields = ('created', 'updated')
+
+    fieldsets = (
+        ('Info', {
+            'fields': ('text', 'user', 'created', 'updated', 'goal')
+        }),
+        ('Dates', {
+            'fields': ('created', 'updated')
+        }),
+    )
 
 
 # ----------------------------------------------------------------
