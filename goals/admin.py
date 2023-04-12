abba60269978a7ca1bdb61c1a2d386490ae8012d
Michael Rodionov <rodionovspb9618@mail.ru>
2023-04-12 22:15:41 +0300

    add goaladmin, commentadmin

--- goals/admin.py
+++ goals/admin.py
@@ -4,10 +4,24 @@ from goals import models
 
 
 # ----------------------------------------------------------------
-# admin register
+# admin models
 class GoalCategoryAdmin(admin.ModelAdmin):
     list_display = ('title', 'user', 'created', 'updated')
     search_fields = ('title', 'user')
 
 
+class GoalAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'created', 'updated', 'category')
+    search_fields = ('title', 'user', 'category')
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('text', 'user', 'created', 'updated', 'goal')
+    search_fields = ('text', 'user', 'goal')
+
+
+# ----------------------------------------------------------------
+# admin register
 admin.site.register(models.GoalCategory, GoalCategoryAdmin)
+admin.site.register(models.Goal, GoalAdmin)
+admin.site.register(models.Comment, CommentAdmin)
