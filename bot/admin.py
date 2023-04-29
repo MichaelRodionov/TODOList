d4aa4e0c01bbe0bac651b684669a2b61227fc0e2
Michael Rodionov <rodionovspb9618@mail.ru>
2023-04-29 22:44:53 +0300

    add admin

--- bot/admin.py
+++ bot/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from bot.models import TgUser
+
+
+# ----------------------------------------------------------------
+# admin models
+@admin.register(TgUser)
+class BoardAdmin(admin.ModelAdmin):
+    list_display = ('tg_chat_id', 'tg_user_id', 'user', 'status')
+
+    fieldsets = (
+        ('Info', {
+            'fields': ('user', 'tg_chat_id', 'tg_user_id')
+        }),
+        ('Status', {
+            'fields': ('status',)
+        }),
+    )
